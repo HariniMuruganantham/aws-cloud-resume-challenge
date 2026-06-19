4f420a0411fab594c3af073523f02973ccbff603
Harini <158560191+HariniMuruganantham@users.noreply.github.com>
2026-06-19 09:43:49 +0000

    revert: remove vite-react-ssg, back to standard vite build

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
-import { ViteReactSSG } from 'vite-react-ssg'
-import { routes } from './routes'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { ErrorBoundary } from './components/ErrorBoundary'
 import './index.css'
+import App from './App.tsx'
 
-export const createRoot = ViteReactSSG({ routes })
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  </StrictMode>,
+)
